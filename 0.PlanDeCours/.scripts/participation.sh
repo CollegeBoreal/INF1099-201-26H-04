b26bf447b2f8804d68adf585ebb25b7093ce93c0
B300098957 <300098957@collegeboreal.ca>
2026-01-14 10:07:38 -0500

    .

--- 0.PlanDeCours/.scripts/participation.sh
+++ 0.PlanDeCours/.scripts/participation.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # --------------------------------------
-#
-#
-#
+# Student Participation Report
 # --------------------------------------
 
+# Load the combined students array
 source ../.scripts/students.sh --source-only
-   
-echo "# Participation au `date +"%d-%m-%Y %H:%M"`"
-echo ""
 
+echo "# Participation au $(date +"%d-%m-%Y %H:%M")"
+echo ""
 
 echo "| Table des matières            | Description                                             |"
 echo "|-------------------------------|---------------------------------------------------------|"
@@ -20,12 +18,10 @@ echo "| :b: [Précision](#b-précision) | L'étudiant.e a réussi son travail  :
 echo ""
 echo "## Légende"
 echo ""
-
 echo "| Signe              | Signification                 |"
 echo "|--------------------|-------------------------------|"
 echo "| :heavy_check_mark: | Prêt à être corrigé           |"
 echo "| :x:                | Projet inexistant             |"
-
 
 echo ""
 echo "## :a: Présence"
@@ -36,33 +32,32 @@ echo "|------|----------------------------|--------------|--------|"
 i=0
 s=0
 
-for id in "${ETUDIANTS[@]}"
-do
-   URL="[${IDS[${i}]}](https://github.com/${IDS[${i}]}) <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image>"
-   FILE=${id}/README.md
-   FOLDER=${id}/images
-   OK="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :heavy_check_mark: | :x: |"
-   FULL_OK="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :heavy_check_mark: | :heavy_check_mark: | "
-   KO="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :x: | :x: |"
-   if [ -f "$FILE" ]; then
-    ACTUAL_NAME="$(basename "$(realpath "$FILE")")"
-    if [[ "$ACTUAL_NAME" == "README.md" ]]; then
+for entry in "${STUDENTS[@]}"; do
+    IFS='|' read -r ID GITHUB AVATAR <<< "$entry"
+
+    URL="[${GITHUB}](https://github.com/${GITHUB}) <image src='https://avatars0.githubusercontent.com/u/${AVATAR}?s=460&v=4' width=20 height=20></image>"
+    FILE=${ID}/README.md
+    FOLDER=${ID}/images
+    OK="| ${i} | [${ID}](../${FILE}) :point_right: ${URL} | :heavy_check_mark: | :x: |"
+    FULL_OK="| ${i} | [${ID}](../${FILE}) :point_right: ${URL} | :heavy_check_mark: | :heavy_check_mark: |"
+    KO="| ${i} | [${ID}](../${FILE}) :point_right: ${URL} | :x: | :x: |"
+
+    if [ -f "$FILE" ]; then
         if [ -d "$FOLDER" ]; then
-                echo ${FULL_OK}
-                let "s++"
+            echo ${FULL_OK}
+            ((s++))
         else
             echo ${OK}
         fi
     else
-       echo ${KO}
+        echo ${KO}
     fi
-   else
-       echo ${KO}
-   fi
-   let "i++"
-   COUNT="\$\\frac{${s}}{${i}}$"
-   STATS=$(echo "$s*100/$i" | bc)
-   SUM="$\displaystyle\sum_{i=1}^{${i}} s_i$"
- done
- 
-echo "| :abacus: | " ${COUNT} " = " ${STATS}% "|" ${SUM} = ${s} "|"
+
+    ((i++))
+    COUNT="\$\\frac{${s}}{${i}}$"
+    STATS=$(echo "$s*100/$i" | bc)
+    SUM="$\displaystyle\sum_{i=1}^{${i}} s_i$"
+done
+
+echo "| :abacus: | ${COUNT} = ${STATS}% | ${SUM} = ${s} |"
+
