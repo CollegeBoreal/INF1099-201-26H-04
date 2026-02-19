4ed8ea66b0a51b407ded35f16472a2583d79d972
Diallo <mamadouabdoulayediallo52@gmail.com>
2026-02-19 03:20:27 -0500

    updates

--- 5.ModelisationSQL/300147786/DCL.sql
+++ 5.ModelisationSQL/300147786/DCL.sql
@@ -1,16 +1,16 @@
 -- Création d’un utilisateur PostgreSQL
-CREATE USER utilisateur_app WITH PASSWORD 'password123';
+CREATE USER utilisateur_1 WITH PASSWORD 'password123';
 
 -- Donner les droits
-GRANT CONNECT ON DATABASE votre_base TO utilisateur_app;
-GRANT USAGE ON SCHEMA public TO utilisateur_app;
+GRANT CONNECT ON DATABASE Colocation TO utilisateur_1;
+GRANT USAGE ON SCHEMA public TO utilisateur_1;
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO utilisateur_app;
+TO utilisateur_1;
 
 -- Pour futures tables
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT, INSERT, UPDATE, DELETE
-ON TABLES TO utilisateur_app;
+ON TABLES TO utilisateur_1;
 
