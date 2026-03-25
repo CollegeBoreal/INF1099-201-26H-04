d605fd571fdd65041065d0b91783b2cffaddde80
Corneil Ekofo Wema <corneilekofo003@gmail.com>
2026-03-25 13:55:36 -0400

    changement script

--- 6.BATCH/300150562/load-db.ps1
+++ 6.BATCH/300150562/load-db.ps1
@@ -3,36 +3,39 @@ $docker = "C:\Program Files\Docker\Docker\resources\bin\docker.exe"
 $container = "postgres-maillot"
 $db = "boutique_maillots"
 $user = "postgres"
-$log = "execution.log"
 
-Write-Host "🚀 Début du chargement..."
+# fichier log en TXT
+$log = "execution.txt"
+
+Start-Transcript -Path $log -Append
+
+Write-Host "[START] Debut du chargement..."
 
 $running = & $docker ps --format "{{.Names}}"
 if ($running -notcontains $container) {
-    Write-Host "❌ Conteneur non actif"
+    Write-Host "[ERROR] Conteneur non actif"
+    Stop-Transcript
     exit
 }
 
-# Nettoyer log
-if (Test-Path $log) {
-    Remove-Item $log
-}
-
-# Copier fichiers dans le conteneur
+Write-Host "[INFO] Copie des fichiers..."
 & $docker cp DDL.sql ${container}:/DDL.sql
 & $docker cp DML.sql ${container}:/DML.sql
 & $docker cp DCL.sql ${container}:/DCL.sql
 & $docker cp DQL.sql ${container}:/DQL.sql
 
-# Exécution avec arrêt si erreur
 function Run-SQL($file) {
-    Write-Host "📄 Exécution $file"
+    Write-Host "[INFO] Execution $file"
+
     $result = & $docker exec -i $container psql -U $user -d $db -f "/$file" 2>&1
 
-    $result | Tee-Object -FilePath $log -Append
+    $result | ForEach-Object {
+        Write-Host $_
+    }
 
     if ($LASTEXITCODE -ne 0) {
-        Write-Host "❌ ERREUR dans $file"
+        Write-Host "[ERROR] Probleme dans $file"
+        Stop-Transcript
         exit
     }
 }
@@ -42,4 +45,6 @@ Run-SQL "DML.sql"
 Run-SQL "DCL.sql"
 Run-SQL "DQL.sql"
 
-Write-Host "✅ Terminé avec succès !"
+Write-Host "[OK] Termine avec succes !"
+
+Stop-Transcript
