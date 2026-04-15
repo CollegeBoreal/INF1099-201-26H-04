1723e5eb48a2006d92bf7bb934e3cd2b7bd4c0bc
mazighs <barachemazighoo@.com>
2026-04-14 23:35:15 -0400

    improve sql for validation

--- 5.ModelisationSQL/300150271/dcl.sql
+++ 5.ModelisationSQL/300150271/dcl.sql
@@ -1,9 +1,9 @@
-CREATE USER agent WITH PASSWORD '1234';
+CREATE USER technicien_user WITH PASSWORD 'tech123';
+CREATE USER gestionnaire_user WITH PASSWORD 'gest123';
 
-GRANT CONNECT ON DATABASE postgres TO agent;
-GRANT USAGE ON SCHEMA public TO agent;
+GRANT CONNECT ON DATABASE postgres TO technicien_user, gestionnaire_user;
+GRANT USAGE ON SCHEMA public TO technicien_user, gestionnaire_user;
 
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO agent;
-GRANT INSERT ON ALL TABLES IN SCHEMA public TO agent;
-GRANT UPDATE ON ALL TABLES IN SCHEMA public TO agent;
-GRANT DELETE ON ALL TABLES IN SCHEMA public TO agent;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO technicien_user;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO gestionnaire_user;
--- 5.ModelisationSQL/300150271/dql.sql
+++ 5.ModelisationSQL/300150271/dql.sql
@@ -1,8 +1,3 @@
-SELECT * FROM Client;
-SELECT * FROM Immeuble;
-SELECT * FROM Appartement;
-SELECT * FROM Vente;
-
 SELECT c.Nom, v.DateVente
 FROM Client c
 JOIN Vente v ON c.IdClient = v.IdClient;
