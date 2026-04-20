5cbb56a9fb371f0a814529de60af7fd1dd48feea
B300098957 <300098957@collegeboreal.ca>
2026-04-20 10:32:44 -0400

    .

--- .scripts/grading/push_grades.ps1
+++ .scripts/grading/push_grades.ps1
@@ -10,11 +10,8 @@ $LMSStudents = Get-LMSStudentInfo -LMSResponse $responseLMS
 foreach ($entry in $STUDENTS) {
     $parts = $entry -split '\|'
     $StudentID = $parts[0]
-    $GitHubID  = $parts[1]
-    $AvatarID  = $parts[2]
 
     Write-Output $LMSStudents[$StudentID].moodleId
-    # Write-Output $LMSStudents[$StudentID].email
 
 }
 
